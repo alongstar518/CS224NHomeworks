f9e73099fe9f6edad7f0a524908f692b0b1353e6
ZhengNie <alongstar518@hotmail.com>
2019-01-31 15:09:01 -0800

    finsh 1(a)

--- homework4/a4/a4/utils.py
+++ homework4/a4/a4/utils.py
@@ -29,8 +29,8 @@ def pad_sents(sents, pad_token):
     sents_padded = []
 
     ### YOUR CODE HERE (~6 Lines)
-
-
+    max_len = max(list(map(lambda x: len(x), sents)))
+    sents_padded = list(map(lambda x: x + ([pad_token] * (max_len - len(x))), sents))
     ### END YOUR CODE
 
     return sents_padded
