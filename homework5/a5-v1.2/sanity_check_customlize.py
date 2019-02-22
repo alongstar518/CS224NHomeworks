bda39e17bef0cbe2aa6d1af1181ef26cfa094971
ZhengNie <alongstar518@hotmail.com>
2019-02-22 12:03:49 -0800

    final submitted

--- homework5/a5-v1.2/sanity_check_customlize.py
+++ homework5/a5-v1.2/sanity_check_customlize.py
@@ -2,5 +2,19 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-from char_decoder import CharDecoder
+from cnn import CNN
+
+in_channel= 5
+out_channel = 2
+k = 5
+hw = CNN(size1, size2)
+
+assert hw.cov1d_layer.in_channels == in_channel
+assert hw.cov1d_layer.out_channels == out_channel
+assert hw.cov1d_layer.kernel_size == k
+
+t = torch.randn((5,5))
+out = hw(t)
+assert t.size() == out.size()
+
 
